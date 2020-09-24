5ff529093ac8ae133be1c46d512b21fef0658c6c
RedCAT <akaineko18@gmail.com>
2020-09-24 15:45:01 +1200

    Updates

---
+++ precache-manifest.391878e189c97988c0f5b14534e85997.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae6b93d001f5fae8cb1763340d6d4744",
+    "url": "/nwitter/index.html"
+  },
+  {
+    "revision": "875f48f7f417b1d79f9b",
+    "url": "/nwitter/static/css/main.9670c0fe.chunk.css"
+  },
+  {
+    "revision": "931a1cc29a4f4ca58665",
+    "url": "/nwitter/static/js/2.0460f41f.chunk.js"
+  },
+  {
+    "revision": "6524af321f8d3e987e59afab1834492e",
+    "url": "/nwitter/static/js/2.0460f41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875f48f7f417b1d79f9b",
+    "url": "/nwitter/static/js/main.d793ce3a.chunk.js"
+  },
+  {
+    "revision": "97fb235846154051176c",
+    "url": "/nwitter/static/js/runtime-main.222ae764.js"
+  }
+]);
